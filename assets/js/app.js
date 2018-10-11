dc32921e613a1318f2665e59e0308810c944ab12
Tom F <thomas.flitterman@ynov.com>
2018-10-11 17:29:51 +0200

    Set up Encore

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,6 +8,12 @@
 // any CSS you require will output into a single css file (app.css in this case)
 require('../css/app.css');
 
+const $ = require('jquery');
+require('bootstrap');
+$(document).ready(function() {
+    $('[data-toggle="popover"]').popover();
+});
+
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // var $ = require('jquery');
 
