396d037735888ab94db2f755d9cc3abf7bf273fb
Tom F <thomas.flitterman@ynov.com>
2018-10-16 23:46:22 +0200

    Updated user page, added Message entity

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Entity\Friends;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,13 +14,90 @@ class UserController extends AbstractController
      */
     public function index(string $username)
     {
+        $currentUser = $this->getUser();
+
         $user = $this->getDoctrine()
             ->getRepository(User::class)
             ->findOneBy(['userName' => $username])
         ;
 
+        $subscribed = false;
+
+        if ($currentUser) {
+            $relation = $this->getDoctrine()
+                ->getRepository(Friends::class)
+                ->findOneBy([
+                    'follower' => $currentUser,
+                    'id_followed' => $user->getId()
+                ]);
+
+            if ($relation) {
+                $subscribed = true;
+            } else {
+                $subscribed = false;
+            }
+        }
+
+        // get the number of subscribers
+        $subscribers = $this->getDoctrine()
+            ->getRepository(Friends::class)
+            ->findBy(['id_followed' => $user->getId()]);
+        $nbSubscribers = count($subscribers);
+
+        // get the number of subscriptions
+        $subscriptions = $this->getDoctrine()
+            ->getRepository(Friends::class)
+            ->findBy(['follower' => $user]);
+        $nbSubscriptions = count($subscriptions);
+
+
         return $this->render('user/index.html.twig', [
-            'user' => $user
+            'user' => $user,
+            'current_user' => $currentUser,
+            'subscribed' => $subscribed,
+            'nb_subscribers' => $nbSubscribers,
+            'nb_subscriptions' => $nbSubscriptions
         ]);
+    }
+
+    /**
+     * @Route("/subscribe/{id}", name="subscribe")
+     */
+    public function subscribe(int $id) {
+        $friendsRow = new Friends();
+        $friendsRow->setFollower($this->getUser());
+        $friendsRow->setIdFollowed($id);
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($friendsRow);
+        $entityManager->flush();
+
+        $userTarget = $this->getDoctrine()
+            ->getRepository(User::class)
+            ->findOneBy(['id' => $id]);
+
+        return $this->redirectToRoute('user', ['username' => $userTarget->getUsername()]);
+    }
+
+    /**
+     * @Route("/unsubscribe/{id}", name="unsubscribe")
+     */
+    public function unsubscribe(int $id) {
+        $friendsRowToDelete = $this->getDoctrine()
+            ->getRepository(Friends::class)
+            ->findOneBy([
+                'follower' => $this->getUser(),
+                'id_followed' => $id
+            ]);
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($friendsRowToDelete);
+        $entityManager->flush();
+
+        $redirectUserTarget = $this->getDoctrine()
+            ->getRepository(User::class)
+            ->findOneBy(['id' => $id]);
+
+        return $this->redirectToRoute('user', ['username' => $redirectUserTarget->getUsername()]);
     }
 }
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -40,6 +40,50 @@ class AppFixtures extends Fixture
         $user2->setSchool($school1);
         $manager->persist($user2);
 
+        $user3 = new User();
+        $user3->setFirstName('Marilyn');
+        $user3->setLastName('Monroe');
+        $user3->setUserName('marilynmonroe');
+        $user3->setEmail('marilyn.monroe@ynov.com');
+        $user3->setProfilePic('default_profile.png');
+        $user3->setBio('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vulputate vel ex eget ultrices. Praesent lacus turpis, pharetra placerat suscipit at, vulputate vel metus. Vivamus ut elit purus.');
+        $user3->setPassword('$2y$13$HPn8PXzrlJFt4mgoj3vJg.HbqiGDuLYFXRNuBjSc6F4L/LhXy/716');
+        $user3->setSchool($school1);
+        $manager->persist($user3);
+
+        $user4 = new User();
+        $user4->setFirstName('John');
+        $user4->setLastName('Wayne');
+        $user4->setUserName('johnwayne');
+        $user4->setEmail('john.wayne@ynov.com');
+        $user4->setProfilePic('default_profile.png');
+        $user4->setBio('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vulputate vel ex eget ultrices. Praesent lacus turpis, pharetra placerat suscipit at, vulputate vel metus. Vivamus ut elit purus.');
+        $user4->setPassword('$2y$13$HPn8PXzrlJFt4mgoj3vJg.HbqiGDuLYFXRNuBjSc6F4L/LhXy/716');
+        $user4->setSchool($school1);
+        $manager->persist($user4);
+
+        $user5 = new User();
+        $user5->setFirstName('Rita');
+        $user5->setLastName('Hayworth');
+        $user5->setUserName('ritahayworth');
+        $user5->setEmail('rita.hayworth@ynov.com');
+        $user5->setProfilePic('default_profile.png');
+        $user5->setBio('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vulputate vel ex eget ultrices. Praesent lacus turpis, pharetra placerat suscipit at, vulputate vel metus. Vivamus ut elit purus.');
+        $user5->setPassword('$2y$13$HPn8PXzrlJFt4mgoj3vJg.HbqiGDuLYFXRNuBjSc6F4L/LhXy/716');
+        $user5->setSchool($school1);
+        $manager->persist($user5);
+
+        $user6 = new User();
+        $user6->setFirstName('Clint');
+        $user6->setLastName('Eastwood');
+        $user6->setUserName('clinteastwood');
+        $user6->setEmail('clint.eastwood@ynov.com');
+        $user6->setProfilePic('default_profile.png');
+        $user6->setBio('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vulputate vel ex eget ultrices. Praesent lacus turpis, pharetra placerat suscipit at, vulputate vel metus. Vivamus ut elit purus.');
+        $user6->setPassword('$2y$13$HPn8PXzrlJFt4mgoj3vJg.HbqiGDuLYFXRNuBjSc6F4L/LhXy/716');
+        $user6->setSchool($school1);
+        $manager->persist($user6);
+
         $manager->flush();
     }
 }
