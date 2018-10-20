a29001059a6938dae09de3334de270bfcb42d0c3
Tom F <thomas.flitterman@ynov.com>
2018-10-20 23:08:08 +0200

    Update profile page

--- src/Controller/UpdateProfileController.php
+++ src/Controller/UpdateProfileController.php
@@ -3,17 +3,41 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\UpdateProfileType;
+use App\Entity\User;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UpdateProfileController extends AbstractController
 {
     /**
-     * @Route("/update/profile", name="update_profile")
+     * @Route("/settings/profile", name="update_profile")
      */
-    public function index()
+    public function index(Request $request, UserPasswordEncoderInterface $encoder)
     {
+        $user = $this->getUser();
+
+        $form = $this->createForm(UpdateProfileType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() AND $form->isValid()) {
+            $updatedUser = new User();
+            $updatedUser = $form->getData();
+
+            $encrypted = $encoder->encodePassword($updatedUser, $updatedUser->getPassword());
+            $updatedUser->setPassword($encrypted);
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($updatedUser);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('user', ['username' => $updatedUser->getUsername()]);
+
+        }
+
         return $this->render('update_profile/index.html.twig', [
-            'controller_name' => 'UpdateProfileController',
+            'form' => $form->createView()
         ]);
     }
 }
